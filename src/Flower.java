dbd1c112814e6a42119f4af44397ac2e7714dc74
Пользователь <woodoo08@mail.ru>
2022-11-10 21:16:19 +0300

    первый коммит

---
+++ src/Flower.java
@@ -0,0 +1,89 @@
+public class Flower {
+    private String flowerColor;
+    private String country;
+    private float cost;
+    int lifeSpan;
+    private final String flowerName;
+
+    public Flower(String flowerName, String flowerColor, String country, float cost) {
+        this.flowerName = flowerName;
+        if (flowerColor == null || flowerColor.length() == 0) {
+            this.flowerColor = "белый";
+        } else {
+            this.flowerColor = flowerColor;
+        }
+        if (country == null || country.length() == 0) {
+            this.country = "Россия";
+        } else {
+            this.country = country;
+        }
+        if (cost <= 0) {
+            this.cost = 1;
+        } else {
+            this.cost = cost;
+        }
+        this.lifeSpan = 1;
+    }
+
+    public Flower(String flowerName, String flowerColor, String country, float cost, int lifeSpan) {
+        this.flowerName = flowerName;
+        if (flowerColor == null || flowerColor.length() == 0) {
+            this.flowerColor = "белый";
+        } else {
+            this.flowerColor = flowerColor;
+        }
+        if (country == null || country.length() == 0) {
+            this.country = "Россия";
+        } else {
+            this.country = country;
+        }
+        if (cost <= 0) {
+            this.cost = 1;
+        } else {
+            this.cost = cost;
+        }
+        if (lifeSpan <= 0) {
+            this.lifeSpan = 3;
+        } else {
+            this.lifeSpan = lifeSpan;
+        }
+    }
+
+    public String getFlowerName() {
+        return flowerName;
+    }
+
+    public String getFlowerColor() {
+        return flowerColor;
+    }
+
+    public void setFlowerColor(String flowerColor) {
+        this.flowerColor = flowerColor;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Цветок: " + flowerName +
+                ". Цвет цветка: "  + flowerColor +
+                ". Страна происхождения: " + country +
+                ". Цена: " + cost +
+                ". Данный цветок простоит " + lifeSpan +
+                " дней!";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void allFlowers(Flower[] flowers) {
+        for (Flower item : flowers) {
+            System.out.println(item.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Flower[] flowers = new Flower[4];
+        flowers[0] = new Flower("Роза обыкновенная", "", "Голландия",
+                35.59f);
+        flowers[1] = new Flower("Хризантема", "", "",
+                15.00f, 5);
+        flowers[2] = new Flower("Пион", "", "Англия",
+                69.9f, 1);
+        flowers[3] = new Flower("Гипсофила", "", "Турция",
+                19.5f, 10);
+
+        allFlowers(flowers);
+
+
+    }
+}
